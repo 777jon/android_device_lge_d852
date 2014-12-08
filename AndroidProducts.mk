3d3d8ca6cfeadd433e6139d0049889a02bd7a466
777jon <jcruzdeveloper@gmail.com>
2014-12-08 11:21:42 -0500

    d852: TWRP bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2014 The CyanogenMod Project
+# Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,15 +14,5 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from d852 device
-$(call inherit-product, device/lge/d852/device.mk)
-
-# Set those variables here to overwrite the inherited values.
-PRODUCT_DEVICE := d852
-PRODUCT_NAME := full_d852
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-D852
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_d852.mk
